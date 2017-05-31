f61991f54b2a852a129356bb784e9714ce2b5d52
mbianka89 <matyas.bianka28@gmail.com>
2017-05-31 14:47:37 +0300

    Create reportcard.java

---
+++ reportcard.java
@@ -0,0 +1,84 @@
+package com.example.android.reportcardapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.Arrays;
+
+public class ReportCard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+// It`s a Report Card used by only one IT teacher, using Hungarian grading system from 1-5, where 5 is the best grade.
+
+
+        // Name of the student
+        private String mStudentsName;
+
+        // Subjects of the student
+        private String mSubjects[];
+
+        // Numeric grades for subjects
+        private int mNumericGrades[];
+
+
+
+        // Constant is the IT teacher, who is the only admin of this report card.
+
+        private static final String IT_TEACHER = "IT Teacher";
+
+        public ReportCard (int[] numericGrades, String[] subjects, String studentsName) {
+
+            this.mStudentsName = studentsName;
+            this.mSubjects = subjects;
+            this.mNumericGrades = numericGrades ;
+        }
+
+        public String getmStudentsName(){
+
+            return mStudentsName;
+        }
+
+        public void setmStudentsName(String studentsName){
+
+            this.mStudentsName = studentsName;
+        }
+
+        public int[] getmNumericGrades(){
+
+            return mNumericGrades;
+        }
+
+        public void setNumericGrades (int[] NumericGrades){
+
+            this.mNumericGrades = mNumericGrades;
+        }
+
+        public String[] getSubjects(){
+
+            return mSubjects;
+        }
+
+        public void setSubjects(String[] subjects){
+
+            this.mSubjects = subjects;
+        }
+
+
+        @Override
+        public String toString(){
+
+            return "IT Teacher: " + IT_TEACHER + "\n"
+                    + "Student`s name: " + mStudentsName + "\n"
+                    + "Student`s subjects: " + Arrays.toString(mSubjects) + " " + "Student`s grade: " + Arrays.toString(mNumericGrades);
+        }
+
+    }
+
+
